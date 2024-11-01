464c77052b615fc4e59de1be9ab0fff9bc9e3cc5
Pete Johns <paj+github@johnsy.com>
2024-11-01 13:04:41 +1100

    Moving forward

--- src/models/EventAmbassador.ts
+++ src/models/EventAmbassador.ts
@@ -1,9 +1,7 @@
 import { EventTeam } from "./EventTeam";
-import { RegionalAmbassador } from "./RegionalAmbassador";
 
 export interface EventAmbassador {
   name: string;
   events: string[];
   supportedEventTeams?: EventTeam[];
-  regionalAmbassador?: RegionalAmbassador;
 }
--- src/models/RegionalAmbassadorMap.ts
+++ src/models/RegionalAmbassadorMap.ts
@@ -1,3 +1,15 @@
 import { RegionalAmbassador } from './RegionalAmbassador';
 
 export type RegionalAmbassadorMap = Map<string, RegionalAmbassador>;
+
+export function getRegionalAmbassador(
+  eventAmbassadorName: string,
+  reverseLookupMap: Map<string, string>,
+  regionalAmbassadorsMap: RegionalAmbassadorMap
+): RegionalAmbassador | undefined {
+  const raName = reverseLookupMap.get(eventAmbassadorName);
+  if (raName) {
+    return regionalAmbassadorsMap.get(raName);
+  }
+  return undefined;
+}
