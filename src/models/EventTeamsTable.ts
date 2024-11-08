0f5bd445f5e6a5bcebf7a2da26b39f9d29739315
Pete Johns <paj+github@johnsy.com>
2024-11-09 06:30:36 +1100

    Do the fetch once

--- src/models/EventTeamsTable.ts
+++ src/models/EventTeamsTable.ts
@@ -9,25 +9,29 @@ export function extractEventTeamsTableData(
   regionalAmbassadors: RegionalAmbassadorMap,
   eventAmbassadors: EventAmbassadorMap,
   eventTeams: EventTeamMap,
-  eventDetails: EventDetailsMap
+  eventDetailsMap: EventDetailsMap
 ): EventTeamsTableDataMap {
   const data: EventTeamsTableDataMap = new Map<string, EventTeamsTableData>();
 
   regionalAmbassadors.forEach((ra, raName) => {
     ra.supportsEAs.forEach((ea) => {
       eventAmbassadors.get(ea)?.events.forEach((eventName) => {
+        const eventDetails = eventDetailsMap.get(eventName);
+        if (!eventDetails) {
+          console.error(`Event details not found for ${eventName}`);
+        }
         const eventTeam = eventTeams.get(eventName);
         const countryCode =
-          eventDetails.get(eventName)?.properties.countrycode ?? 0;
+          eventDetails?.properties.countrycode ?? 0;
         data.set(eventName, {
           eventShortName: eventName,
           eventDirectors: eventTeam?.eventDirectors.join(", ") ?? "N/A",
           eventAmbassador: ea,
           regionalAmbassador: raName,
           eventCoordinates:
-            eventDetails.get(eventName)?.geometry?.coordinates?.join(", ") ??
+            eventDetails?.geometry?.coordinates?.join(", ") ??
             "N/A",
-          eventSeries: eventDetails.get(eventName)?.properties.seriesid ?? 0,
+          eventSeries: eventDetails?.properties.seriesid ?? 0,
           eventCountryCode: countryCode,
           eventCountry: countries[countryCode]?.url?.split('.').slice(-1)[0] ?? "N/A",
         });
