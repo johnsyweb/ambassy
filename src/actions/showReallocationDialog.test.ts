8bf2fdf86a1ffdfb0daef83dfa54554cb0275072
Pete Johns <paj+github@johnsy.com>
2026-01-12 17:03:21 +1100

    fix: remove unused RegionalAmbassadorMap import from test file

--- src/actions/showReallocationDialog.test.ts
+++ src/actions/showReallocationDialog.test.ts
@@ -1,7 +1,6 @@
 import { showReallocationDialog } from "./showReallocationDialog";
 import { ReallocationSuggestion } from "@models/ReallocationSuggestion";
 import { EventAmbassadorMap } from "@models/EventAmbassadorMap";
-import { RegionalAmbassadorMap } from "@models/RegionalAmbassadorMap";
 
 jest.mock("./getReallocationSuggestions");
 
