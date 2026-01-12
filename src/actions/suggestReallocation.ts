547555a87a12e8ddf631025b8137c6106b18c32f
Pete Johns <paj+github@johnsy.com>
2026-01-12 16:46:25 +1100

    fix: change score from const to let to allow modification for prioritization

--- src/actions/suggestReallocation.ts
+++ src/actions/suggestReallocation.ts
@@ -258,7 +258,7 @@ export function suggestEventReallocation(
       return;
     }
 
-    const score = calculateReallocationScore(
+    let score = calculateReallocationScore(
       recipient,
       recipientName,
       events,
