a81f93352804d53927348e44367264c6fd893c12
Pete Johns <paj+github@johnsy.com>
2026-06-16 21:32:25 +1000

    feat(state): extend application state schema to 2.0.0
    Add prospective events and ambassador visit histories to the export file
    model while keeping legacy 1.0.0 import support.

--- src/models/ApplicationState.ts
+++ src/models/ApplicationState.ts
@@ -4,6 +4,18 @@ import { RegionalAmbassador } from "./RegionalAmbassador";
 import { LogEntry } from "./LogEntry";
 import { CapacityLimits } from "./CapacityLimits";
 import { EventDetails } from "./EventDetails";
+import { ProspectiveEvent } from "./ProspectiveEvent";
+import { AmbassadorFinishHistoryMap } from "./AmbassadorFinishHistory";
+
+export const CURRENT_APPLICATION_STATE_VERSION = "2.0.0";
+
+export const SUPPORTED_APPLICATION_STATE_VERSIONS = [
+  "1.0.0",
+  CURRENT_APPLICATION_STATE_VERSION,
+] as const;
+
+export type SupportedApplicationStateVersion =
+  (typeof SUPPORTED_APPLICATION_STATE_VERSIONS)[number];
 
 export interface ApplicationStateData {
   eventAmbassadors: Array<[string, EventAmbassador]>;
@@ -12,10 +24,12 @@ export interface ApplicationStateData {
   changesLog: LogEntry[];
   capacityLimits?: CapacityLimits;
   resolvedEventDetails?: Array<[string, EventDetails]>;
+  prospectiveEvents?: ProspectiveEvent[];
+  ambassadorFinishHistories?: AmbassadorFinishHistoryMap;
 }
 
 export interface ApplicationState {
-  version: string;
+  version: SupportedApplicationStateVersion | string;
   exportedAt: string;
   data: ApplicationStateData;
 }
